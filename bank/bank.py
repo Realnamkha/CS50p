08a830953929f99a086080e14353068b6570f4a0
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-16 08:43:12 +0000

    Mon, Oct 16, 2023, 2:28 PM +05:45

--- bank/bank.py
+++ bank/bank.py
@@ -1,7 +1,7 @@
 Greetings = input("Enter your greetings").lower().strip()
 if Greetings.startswith("hello"):
     print("$0")
-elif Greetings.startswith("H"):
+elif Greetings.startswith("h"):
     print("$20")
 else:
     print("$100")
