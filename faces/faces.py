10c421f99d73b9c5846b5f508fc499a811d461d4
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-16 03:43:09 +0000

    Mon, Oct 16, 2023, 9:28 AM +05:45

--- faces/faces.py
+++ faces/faces.py
@@ -1,1 +1,10 @@
-def convert(text)
+def convert(text):
+    text = text.replace(":)","🙂")
+    text = text.replace(":(","🙁")
+    return text
+def main():
+    sentence = input("Enter your string ")
+    sentence = convert(sentence)
+    print(sentence)
+
+main()
