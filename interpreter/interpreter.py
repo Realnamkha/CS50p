0698a945d195e183a5b9a74306e26c6351642458
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-17 15:49:19 +0000

    Tue, Oct 17, 2023, 9:34 PM +05:45

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -3,10 +3,14 @@ y = input("Enter the operand")
 z = int(input("Enter the second operand"))
 
 if (y=="+"):
-   print(f"{x+z:.1f}")
+   w = str(x + z)
+   print(f"{w:.1f}")
 elif (y=="-"):
-    print(f"{x-z:.1f}")
+    w = str(x - z)
+    print(f"{w:.1f}")
 elif (y=="*"):
-    print(f"{x*z:.1f}")
+    w = str(x * z)
+    print(f"{w:.1f}")
 else:
-    print(f"{x/z:.1f}")
+    w = str(x / z)
+    print(f"{w:.1f}")
