6d8ddf50de8ec92da0a842e5b2219d170206d01a
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-17 15:52:10 +0000

    Tue, Oct 17, 2023, 9:37 PM +05:45

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,16 +1,20 @@
-x = int(input("Enter the first operand"))
-y = input("Enter the operand")
-z = int(input("Enter the second operand"))
+def main():
+    math_interpreter()
 
-if (y=="+"):
-   w = str(x + z)
-   print(f"{w:.1f}")
-elif (y=="-"):
-    w = str(x - z)
-    print(f"{w:.1f}")
-elif (y=="*"):
-    w = str(x * z)
-    print(f"{w:.1f}")
-else:
-    w = str(x / z)
-    print(f"{w:.1f}")
+
+def math_interpreter():
+    x, y, z = input("Expression: ").split()
+    x = float(x)
+    z = float(z)
+    if y == "+":
+        print(f"{x+z:.1f}")
+    elif y == "-":
+        print(f"{x-z:.1f}")
+    if y == "*":
+        print(f"{x*z:.1f}")
+    if y == "/":
+        print(f"{x/z:.1f}")
+
+
+if __name__ == "__main__":
+    main()
