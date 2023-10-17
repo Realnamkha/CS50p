c8cada822c119e652b4816981d6033e07b809a36
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-17 15:38:40 +0000

    Tue, Oct 17, 2023, 9:23 PM +05:45

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,3 +1,3 @@
-x = int(input("Enter the first operand"))
-y = input("Enter the operand")
-z = int(input("Enter the second operand"))
+x = int(input("Enter the first operand")).strip()
+y = input("Enter the operand").strip()
+z = int(input("Enter the second operand")).strip()
