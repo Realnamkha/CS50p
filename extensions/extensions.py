66aacaec181ac486ace88d13a2a4c6b8625422cb
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-17 15:23:37 +0000

    Tue, Oct 17, 2023, 9:08 PM +05:45

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,14 +1,19 @@
 fileinput = input("Enter the file name with extension").lower().strip()
 if ".gif" in fileinput:
     print("image/gif")
+
 elif ".jpg" or "jpeg" in fileinput:
     print("image/jpeg")
+
 elif ".png" in fileinput:
     print("image/png")
+
 elif ".pdf" in fileinput:
     print("image/pdf")
+
 elif ".txt" in fileinput:
     print("image/txt")
+
 elif ".zip" in fileinput:
     print("image/zip")
 else:
