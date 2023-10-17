d1491498bee9a25df2012b4ed2f448804ae46a10
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-17 15:41:01 +0000

    Tue, Oct 17, 2023, 9:26 PM +05:45

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,3 +1,11 @@
 x = int(input("Enter the first operand")).strip()
-y = input("Enter the operand").strip()
+y = input("Enter the operand")
 z = int(input("Enter the second operand")).strip()
+
+if (y=="+"):
+    print(x+z)
+elif (y=="-"):
+    print(x-z)
+elif (y=="*"):
+    print(x*z)
+elif 
