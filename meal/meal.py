a4a636121456de6e9f109b34cc0dab3931fe8c58
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-18 02:36:32 +0000

    Wed, Oct 18, 2023, 8:21 AM +05:45

--- meal/meal.py
+++ meal/meal.py
@@ -2,7 +2,9 @@ hours, minutes = input("Enter the current time ").split(":")
 hours = float(hours)
 minutes = float(minutes)
 minutes = (minutes/60)
-
+l = [hours,minutes]
+s = '.'.join([str(n) for n in l])
+print(s)
 def main():
     ...
 
