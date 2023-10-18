36c0fba6613a136bee7888beab632e55d910cf15
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-18 02:49:59 +0000

    Wed, Oct 18, 2023, 8:34 AM +05:45

--- meal/meal.py
+++ meal/meal.py
@@ -1,21 +1,21 @@
 
 def main():
-    convert(new_time)
-
+    time = input("Enter the current time ")
+    hours = convert(time)
+    if (7 <= hours <= 8):
+        print("Breakfast Time")
+    if (12 <= hours <= 13):
+        print("Lunch Time")
+    if (18 <= hours <= 19):
+        print("Dinner Time")
 
 def convert(time):
-    hours, minutes = input("Enter the current time ").split(":")
+    hours, minutes = time.split(":")
     hours = int(hours)
     minutes = float(minutes)
     minutes = (minutes/60)
     new_time = hours + minutes
     return new_time
 
-if (7 <= time <= 8):
-        print("Breakfast Time")
-if (12 <= time <= 13):
-        print("Lunch Time")
-if (18 <= time <= 19):
-        print("Dinner Time")
 if __name__ == "__main__":
     main()
