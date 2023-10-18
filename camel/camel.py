35de31801166bfb598f438f5c233e1359a9446ee
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-18 03:52:22 +0000

    Wed, Oct 18, 2023, 9:37 AM +05:45

--- camel/camel.py
+++ camel/camel.py
@@ -1,3 +1,4 @@
 camelCase = input("Enter the variable ").strip()
 for i in range(len(camelCase)):
-    if (camelCase[i] == )
+    if (camelCase[i] == camelCase[i].isupper()):
+        print(camelCase[i])
