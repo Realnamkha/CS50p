a797385ee1d6e9ab214baa4a6772764f90b245d8
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-18 04:03:17 +0000

    Wed, Oct 18, 2023, 9:48 AM +05:45

--- camel/camel.py
+++ camel/camel.py
@@ -1,10 +1,13 @@
 camelCase = input("Enter the variable ").strip()
 snake_case = ""
+
 def convert(camelCase):
     for i in range(len(camelCase)):
-        if (camelCase[i].isupper()):
-            smoke_case += "_" + camelCase[i].lower()
+        if camelCase[i].isupper():
+            snake_case += "_" + camelCase[i].lower()
         else:
-            smoke_case += camelCase[i].lower()
-    return smoke_case
-convert(camelCase)
+            snake_case += camelCase[i].lower()
+    return snake_case
+
+snake_case = convert(camelCase)
+print("Snake case:", snake_case)
