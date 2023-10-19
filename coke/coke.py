6d9b7132c41aa4f686c63f76ea5e5e724b0bea7d
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-19 02:38:49 +0000

    Thu, Oct 19, 2023, 8:23 AM +05:45

--- coke/coke.py
+++ coke/coke.py
@@ -4,7 +4,7 @@ while amount_due>0:
     amount_inserted = int(input("Insert Coint :"))
     if amount_inserted in amount_accepted:
         amount_due = amount_due - amount_inserted
-        print(f"Amount Due:{amount_due}cents")
+        print(f"Amount Due:{amount_due} cents")
 if amount_due<=0:
     print(f"Change Owed: {amount_due * -1}")
 
