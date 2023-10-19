aaef10c1d9975b0055e65fc8d0c15dd43db03646
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-19 02:39:35 +0000

    Thu, Oct 19, 2023, 8:24 AM +05:45

--- coke/coke.py
+++ coke/coke.py
@@ -1,7 +1,7 @@
 amount_due = 50
 amount_accepted = [25,10,5]
 while amount_due>0:
-    amount_inserted = int(input("Insert Coint :"))
+    amount_inserted = int(input("Insert Coint :")).strip()
     if amount_inserted in amount_accepted:
         amount_due = amount_due - amount_inserted
         print(f"Amount Due:{amount_due} cents")
