e2a8784db03d9b36258b3967b6986be65ce6ee48
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-19 03:27:24 +0000

    Thu, Oct 19, 2023, 9:12 AM +05:45

--- plates/plates.py
+++ plates/plates.py
@@ -1,3 +1,17 @@
-s= input("Plate: ")
-len(s[:2].isalpha())
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if s[:2].isaplha():
+        return True
+    if(6<=len(s)<=2):
+        return True
+
+
+main()
 
