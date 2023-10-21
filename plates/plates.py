90b13ea518c34341df53d6080f4dfd191b63ccca
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-21 04:44:22 +0000

    Sat, Oct 21, 2023, 10:29 AM +05:45

--- plates/plates.py
+++ plates/plates.py
@@ -7,21 +7,22 @@ def main():
 
 
 def is_valid(s):
+    valid = False
     if(6<=len(s)<=2):
         start = s[:2]
 
         if s.isalpha():
-            return True
+            valid = True
         elif s.isalnum() and start.isalpha():
             if not s[-1].isalpha():
                 for char in s[2:]:
                       if char.isalpha():
                         continue
                       elif char.isnumeric() and char != "0":
-                          return True
+                          valid = True
 
                       break
-
+    return valid
 
 main()
 
