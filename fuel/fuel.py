ddb0bb181d4692f6716d8c4c2ea9f2525b33da77
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-22 08:30:20 +0000

    Sun, Oct 22, 2023, 2:15 PM +05:45

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,16 +1,23 @@
 def main():
-    X = int(input("Enter the value of X"))
-    Y = int(input("Enter the value of Y"))
-    
+    X = int(input("Enter the value of X: "))
+    Y = int(input("Enter the value of Y: "))
+    fuel = get_fuel(X,Y)
+    if (fuel<1):
+        print("E")
+    elif(fuel>99):
+        print("F")
+    else:
+        print(f"{fuel:.0f}%")
+
 def greater(X,Y):
     if X>Y:
         raise Exception("X is greater than Y")
 
-def get_fuel():
+def get_fuel(input1,input2):
     while True:
         try :
-            X = int(input("Enter the value of X"))
-            Y = int(input("Enter the value of Y"))
+            X = input1
+            Y = input2
             fuel = X/Y
             greater(X,Y)
             break
@@ -24,3 +31,5 @@ def get_fuel():
     fuel = fuel *100
     fuel = round(fuel,1)
     return fuel
+
+main()
