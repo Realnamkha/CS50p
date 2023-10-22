e9e18ac818fe330a9deb18ae02bd023354e9855b
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-22 08:15:49 +0000

    Sun, Oct 22, 2023, 2:00 PM +05:45

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,17 @@
+def greater(X,Y):
+    if X>Y:
+        raise Exception("X is greater than Y")
+
+
 try :
-    X = int(input("Enter the value of X))
-    Y = int(input("Enter the value of X))
+    X = int(input("Enter the value of X"))
+    Y = int(input("Enter the value of Y"))
+    greater(X,Y)
 except ValueError:
     print("X or Y is not an integer")
-if X>Y:
-    raise GreaterError()
+except def check_age(age):
+    if age < 18:
+        raise Exception(" Age must be 18 or above. ")
+    else:
+        print(" Age is valid. ")   as e:
+    print(e)
