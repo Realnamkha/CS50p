34ec2178f5a77b4d12c2b63a74ac0ac73e19ddf9
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-22 08:20:16 +0000

    Sun, Oct 22, 2023, 2:05 PM +05:45

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,15 +2,16 @@ def greater(X,Y):
     if X>Y:
         raise Exception("X is greater than Y")
 
-
-try :
-    X = int(input("Enter the value of X"))
-    Y = int(input("Enter the value of Y"))
-    fuel = X/Y
-    greater(X,Y)
-except ValueError:
-    print("X or Y is not an integer")
-except ZeroDivisionError:
-    print("Y cannot be zero")
-except Exception as e:
-    print(e)
+while True:
+    try :
+        X = int(input("Enter the value of X"))
+        Y = int(input("Enter the value of Y"))
+        fuel = X/Y
+        greater(X,Y)
+        break
+    except ValueError:
+        print("X or Y is not an integer")
+    except ZeroDivisionError:
+        print("Y cannot be zero")
+    except Exception as e:
+        print(e)
