d1a6fd763ae5881eae0c89ff7c0f2e78fe99072b
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-23 07:12:07 +0000

    Mon, Oct 23, 2023, 12:57 PM +05:45

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -16,9 +16,9 @@
 date = input("DATE: ")
 if '/' in date:
     month,day,year = date.split('/')
+    print(f"{year}-{month}-{day}")
     if(int(month)>12 or int(day)>31 or int(year)<1):
         raise Exception("Format is incorrect")
 else:
     month,year = date.split(',')
     day,month = month.split(' ')
-    
