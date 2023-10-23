93cf888c921b660ce1f8b6aca5dcfafc5ae0fe54
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-23 07:29:20 +0000

    Mon, Oct 23, 2023, 1:14 PM +05:45

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -12,24 +12,26 @@ months = [
     "November",
     "December"
 ]
-try:
-    date = input("DATE: ")
-    if '/' in date:
-        month,day,year = date.split('/')
-        month = int(month)
-        day = int(day)
-        year = int(year)
-        if(month>12 or day>31 or year<1):
-            raise Exception("Format is incorrect")
-    else:
-        month,year = date.split(',')
-        month,day = month.split(' ')
-        if month in months:
-             month = months.index(month) + 1
-        year = int(year)
-        day = int(day)
-        year = int(year)
-except ValueError():
-    print("ValueError has occrued")
-finally:
-        print(f"{year:04}-{month:02}-{day:02}")
+while True:
+    try:
+        date = input("DATE: ")
+        if '/' in date:
+            month,day,year = date.split('/')
+            month = int(month)
+            day = int(day)
+            year = int(year)
+            if(month>12 or day>31 or year<1):
+                raise Exception("Format is incorrect")
+        else:
+            month,year = date.split(',')
+            month,day = month.split(' ')
+            if month in months:
+                month = months.index(month) + 1
+            year = int(year)
+            day = int(day)
+            year = int(year)
+    except ValueError():
+        print("ValueError has occureed")
+    finally:
+            print(f"{year:04}-{month:02}-{day:02}")
+            break
