148952bbc007828455cf69a9dbc46063a1e02719
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-23 07:19:08 +0000

    Mon, Oct 23, 2023, 1:04 PM +05:45

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -12,13 +12,18 @@
     "November",
     "December"
 ]
-
-date = input("DATE: ")
-if '/' in date:
-    month,day,year = date.split('/')
-    print(f"{year:04}-{month:02}-{day:02}")
-    if(int(month)>12 or int(day)>31 or int(year)<1):
-        raise Exception("Format is incorrect")
-else:
-    month,year = date.split(',')
-    day,month = month.split(' ')
+try:
+    date = input("DATE: ")
+    if '/' in date:
+        month,day,year = date.split('/')
+        month = int(month)
+        day = int(day)
+        year = int(year)
+        print(f"{year:04}-{month:02}-{day:02}")
+        if(month>12 or day>31 or year<1):
+            raise Exception("Format is incorrect")
+    else:
+        month,year = date.split(',')
+        day,month = month.split(' ')
+except ValueError():
+    print("ValueError has occrued")
