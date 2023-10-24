b8eed1663065250fd8d28262ff6ec9091d358176
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-24 15:36:34 +0000

    Tue, Oct 24, 2023, 9:21 PM +05:45

--- game/game.py
+++ game/game.py
@@ -1,10 +1,21 @@
 import random
+import sys
 while True:
     try:
         Level = int(input("Enter a positive number"))
         if(Level<0):
             continue
         number = random.randint(1, Level)
+        Guess = int(input("Guess: "))
+        if(Guess<0):
+            continue
+        if(Guess < number):
+            print("Too small!")
+        elif(Guess > number):
+            print("Too large!")
+        else:
+            print("Just right!")
+            sys.exit()
     except ValueError:
         pass
 
