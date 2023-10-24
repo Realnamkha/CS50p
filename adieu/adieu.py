ab5d0366dd31e8d6376e522cf4efc6e60c9e8fab
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-24 14:58:19 +0000

    Tue, Oct 24, 2023, 8:43 PM +05:45

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -4,3 +4,10 @@ word = "plural"
 print("The plural of ", word, " is ", p.plural(word))
 cat_count = 1
 print("I saw", cat_count, p.plural("cat", cat_count))
+
+print(
+    p.plural_noun("I", N1),
+    p.plural_verb("saw", N1),
+    p.plural_adj("my", N2),
+    p.plural_noun("saw", N2),
+)
