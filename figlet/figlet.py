6c114dd0ac76b6262c9bb8420c7a0c451e0aa72a
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-24 08:13:39 +0000

    Tue, Oct 24, 2023, 1:58 PM +05:45

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,11 +6,16 @@ fonts = figlet.getFonts()
 
 if len(sys.argv) == 1:
     Input = input("Enter the string :")
-    figlet.setFont(font = fonts[random.randint(0, 424)])
-
+    if sys.argv[1] == '-f' or '--font':
+        figlet.setFont(font = fonts[random.randint(0, 424)])
+    else:
+        sys.exit("Invalid Argument")
 elif len(sys.argv) == 3:
     Input = input("Enter the string :")
-    figlet.setFont(font=sys.argv[2])
+    if ((sys.argv[1] == '-f' or '--font') and (argv[2] in fonts)):
+        figlet.setFont(font=sys.argv[2])
+    else:
+        sys.exit("Invalid Argument")
 else:
     sys.exit("Argument few or many")
 
