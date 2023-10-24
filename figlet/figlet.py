a4f89762c113005f1ff0d75a88971b12a3e18f21
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-24 07:58:47 +0000

    Tue, Oct 24, 2023, 1:43 PM +05:45

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,5 +1,14 @@
 from pyfiglet import Figlet
+import sys
+Input = input("Enter the string :")
+if len(sys.argv) == 0:
 
+elif len(sys.argv) == 2:
+
+else:
+    sys.exit("Argument few or many")
+
+print("hello, my name is", sys.argv[1])
 figlet = Figlet()
 print(figlet.getFonts())
 figlet.setFont(font=f)
