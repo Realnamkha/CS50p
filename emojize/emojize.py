9c1f4c8439060bec97c21c1a0121375b71c7ad83
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-24 07:40:06 +0000

    Tue, Oct 24, 2023, 1:25 PM +05:45

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,3 +1,17 @@
+# import emoji
+# Input = input("Input: ")
+# print(emoji.emojize(Input,language = 'en' or 'alias'))
 import emoji
-Input = input("Input: ")
-print(emoji.emojize(Input,language = 'en' or 'alias'))
+
+
+def main():
+    emojize()
+
+
+def emojize():
+    user_input = input("Input: ")
+    print(emoji.emojize(user_input))
+
+
+if __name__ == "__main__":
+    main()
