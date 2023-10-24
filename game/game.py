a8b8b815014bd8b9d2461d8c838b114179ce8802
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-24 15:33:37 +0000

    Tue, Oct 24, 2023, 9:18 PM +05:45

--- game/game.py
+++ game/game.py
@@ -1,8 +1,10 @@
 import random
 while True:
     try:
-        n = int(input("Enter a positive number"))
-        if(n<0):
+        Level = int(input("Enter a positive number"))
+        if(Level<0):
             continue
+        number = random.randint(1, Level)
     except ValueError:
         pass
+
