e9089f9131df1b29cdbabd7539637bda10f12662
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-25 09:54:54 +0000

    Wed, Oct 25, 2023, 3:39 PM +05:45

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,7 +4,7 @@ import json
 while True:
     try:
         n = float(sys.argv[1])
-        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
         rate_float = response["bpi"]["USD"]["rate_float"]
         print(rate_float)
         break
