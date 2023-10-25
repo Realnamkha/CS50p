4d9c1f0de67f6b8ad2ceca30173cf7807cb1af79
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-25 09:42:35 +0000

    Wed, Oct 25, 2023, 3:27 PM +05:45

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,7 +1,11 @@
 import sys
+import requests
+import json
 while True:
     try:
         n = float(sys.argv[1])
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        print(json.dumps(response.json(), indent=2))
         break
     except ValueError:
         sys.exit("Input is not a number")
