db226fd21f299a49861e49c8a86e3a8bd7b01e5b
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-25 08:41:56 +0000

    Wed, Oct 25, 2023, 2:26 PM +05:45

--- professor/professor.py
+++ professor/professor.py
@@ -6,7 +6,12 @@ def main():
 
 
 def get_level():
-    ...
+    level = -1
+    while (level<0):
+        try:
+            level = int(input("Level :"))
+        except ValueError:
+            pass
 
 
 def generate_integer(level):
