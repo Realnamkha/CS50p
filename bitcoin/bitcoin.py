b3c8a2c082c458853dd9f1661d8484519f369ebc
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-25 09:47:07 +0000

    Wed, Oct 25, 2023, 3:32 PM +05:45

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -6,7 +6,8 @@ while True:
         n = float(sys.argv[1])
         response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
         o = response.json()
-        
+        for rate in o["bpi"]:
+            
         break
     except ValueError:
         sys.exit("Input is not a number")
