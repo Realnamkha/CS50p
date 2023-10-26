70528da648aa7b4beba4f71d80ec55fa7190828a
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-26 15:39:08 +0000

    Thu, Oct 26, 2023, 9:24 PM +05:45

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,26 +1,24 @@
 from plates import is_valid
 
-def test_check_start():
-    assert is_valid("CS50") == True
-    assert is_valid("50") == False
+def test_start():
     assert is_valid("a") == False
+    assert is_valid("11") == False
     assert is_valid("1") == False
-    assert is_valid("CS") == True
+    assert is_valid("AA") == True
+    assert is_valid("CS50") == True
 
 
 def test_length():
-    assert is_valid("AA") == True
+    assert is_valid("AAAAAAAAA") == False
+    assert is_valid("AAAA") == True
     assert is_valid("A") == False
-    assert is_valid("AAAAAAA") == False
-    assert is_valid("AAAAAA") == True
-
+    assert is_valid("aaaaaa") == True
 
 def test_numbers():
-    assert is_valid("12345") == False
-    assert is_valid("11AA") == False
     assert is_valid("AA11") == True
-    assert is_valid("AA11A1") == False
-    assert is_valid("AAA11A") == False
+    assert is_valid("11AA") == False
+    assert is_valid("A11AA") == False
+    assert is_valid("AA123") == True
 
 
 def test_zero():
