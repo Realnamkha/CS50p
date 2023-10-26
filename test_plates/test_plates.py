b52523892d4ff24d2c6589d4795d9cbc8a22528e
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-26 15:28:21 +0000

    Thu, Oct 26, 2023, 9:13 PM +05:45

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,1 +1,11 @@
 from plates import is_valid
+
+def test_len():
+    is_valid("A") == False
+    is_valid("AA") == True
+    is_valid("CCCCCC") == False
+    is_valid("AAAAAAAAA") == False
+
+def test_alnumber():
+    is_valid("CS50") == True
+    is_valid("CS05") == False
