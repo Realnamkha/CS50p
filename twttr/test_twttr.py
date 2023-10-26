d5a1bb3f0060a96a46a01198a7cc0aebad0a3b88
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-26 12:13:56 +0000

    Thu, Oct 26, 2023, 5:58 PM +05:45

--- twttr/test_twttr.py
+++ twttr/test_twttr.py
@@ -3,3 +3,5 @@ def test_str():
     assert shorten("aeiou") == ""
     assert shorten("AEIOU") == ""
     assert shorten("bcdz") == "bcdz"
+    assert shorten("1230") =="1230"
+    assert shorten("a,yui") == ",y"
