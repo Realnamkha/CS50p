f554d6a6dec0993eaf1a226cf961dedc1d63922b
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-26 15:03:34 +0000

    Thu, Oct 26, 2023, 8:48 PM +05:45

--- bank/test_bank/test_bank.py
+++ bank/test_bank/test_bank.py
@@ -1,8 +1,15 @@
 from bank import value
 
 def test_value():
-    assert value("hello Namkha") == "$0"
-    assert value("h Namkha") == "$20"
-    # assert value("HELLO NAMKHA") == "$0"
-    assert value("1230") =="$100"
-    assert value("a,yui") == "$100"
+    assert value("What's up?") == "$100"
+
+def test_case_insensitivity():
+    assert value("HELLO") == "$0"
+    assert value("hello") == "$0"
+
+
+def test_all():
+    assert value("Hello, Namkha") == 0
+    assert value("Greetings, Namkha") == 100
+    assert value("Hi, Namkha") == 20
+
