b28cfdef34d9c3b6a0b9b763eb4fbfebd57e6c84
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-26 11:52:39 +0000

    Thu, Oct 26, 2023, 5:37 PM +05:45

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,16 @@
-string = input("Enter the string ").strip()
-vowels = ['a','e','i','o','u','A','E','I','O','U']
-new_string = ""
-for char in string:
-    if char not in vowels:
-        new_string +=char
-print(new_string)
+def main():
+    string = input("Enter the string ").strip()
+    shorten(string)
+
+
+def shorten(word):
+    vowels = ['a','e','i','o','u','A','E','I','O','U']
+    word = ""
+    for char in string:
+        if char not in vowels:
+            word +=char
+    return word
+
+
+if __name__ == "__main__":
+    main()
