4f69eb86f4204377e82d061631ed6c1cba7cdb64
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-26 12:28:19 +0000

    Thu, Oct 26, 2023, 6:13 PM +05:45

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,8 +1,8 @@
-from bank import value
+from bank import bank.py
 
 def test_value():
-    assert value("hello Namkha") == "$0"
-    assert value("h Namkha") == "$20"
-    assert shorten("HELLO NAMKHA") == "$0"
-    assert shorten("1230") =="$100"
-    assert shorten("a,yui") == "$100"
+    assert bank.value("hello Namkha") == "$0"
+    assert bank.value("h Namkha") == "$20"
+    assert bank.value("HELLO NAMKHA") == "$0"
+    assert bank.value("1230") =="$100"
+    assert bank.value("a,yui") == "$100"
