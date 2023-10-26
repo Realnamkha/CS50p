88baaa4f73977e6c7bbe37eda6f83267be501d43
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-26 12:23:16 +0000

    Thu, Oct 26, 2023, 6:08 PM +05:45

--- bank/bank.py
+++ bank/bank.py
@@ -1,7 +1,15 @@
-Greetings = input("Enter your greetings").lower().strip()
-if Greetings.startswith("hello"):
-    print("$0")
-elif Greetings.startswith("h"):
-    print("$20")
-else:
-    print("$100")
+def main():
+    Greetings = input("Enter your greetings: ").lower().strip()
+    print(value(Greetings))
+
+def value(Greetings):
+    if Greetings.startswith("hello"):
+        return "$0"
+    elif Greetings.startswith("h"):
+        return "$20"
+    else:
+        return "$100"
+
+
+if __name__ == "__main__":
+    main()
