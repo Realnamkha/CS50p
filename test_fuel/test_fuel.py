d34ad1cca3cdf97a09917defd7b209c517de8a36
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-27 07:40:04 +0000

    Fri, Oct 27, 2023, 1:25 PM +05:45

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,5 +1,5 @@
 from fuel import convert, gauge
-
+import pytest
 def test_value():
     with pytest.raises(ValueError):
         convert("cats/dogs")
@@ -8,3 +8,14 @@ def test_zero_division():
     with pytest.raises(ZeroDivisionError):
         convert("4/0")
 
+# def test_greater():
+#     with pytest.raises(e):
+#         convert("3/1")
+
+def test_gauge():
+    assert gauge(0) == "E"
+    assert gauge(1) == "E"
+    assert gauge(10) == "10%"
+    assert gauge(90) == "90%"
+    assert gauge(99) == "F"
+    assert gauge(100) == "F"
