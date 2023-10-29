66c6548a3704cc18c03444aa653fd1310ad7b28f
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-29 04:34:30 +0000

    Sun, Oct 29, 2023, 10:19 AM +05:45

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -7,33 +7,29 @@ def get_filename(arguments):
         if len(arguments) < 2:
             exit('Too few command-line arguments')
         else:
-            exit('Too mant command-line arguments')
+            exit('Too many command-line arguments')
     else:
-        return argv[1]
+        return sys.argv[1]
 
 
 def check_csv(file_name):
      if (file_name[-4:] != ".csv"):
          sys.exit("Not a csv file")
+
 def main():
-    filename = get_filename(argv)
-    check_csv(filename)
-try:
-    if len(sys.argv) == 2:
-        file_name = sys.argv[1]
-        if (file_name[-4:] == ".csv"):
-            with open(file_name) as file:
-                 reader = csv.DictReader(file)
-                 for row in reader:
-                    menu.append({"Regular Pizza": row["Regular Pizza"],
-                                 "Small": row["Small"],
-                                 "Large":row["Large"]})
+    file_name = get_filename(sys.argv)
+    check_csv(file_name)
+    try:
+        with open(file_name) as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                menu.append({"Regular Pizza": row["Regular Pizza"],
+                            "Small": row["Small"],
+                            "Large":row["Large"]})
 
-            print(tabulate((menu),headers="keys",tablefmt="grid"))
-        else:
-            sys.exit("Not a CSV file")
-    else:
-        sys.exit("More or few arguments")
-except FileNotFoundError:
-    sys.exit("File does not exist")
+        print(tabulate((menu),headers="keys",tablefmt="grid"))
+    except FileNotFoundError:
+            sys.exit("File does not exist")
 
+if __name__ == '__main__':
+    main()
