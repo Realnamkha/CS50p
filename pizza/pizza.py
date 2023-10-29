7ae9c930b8b1877de0eef0b3303c5434476289a4
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-29 03:56:15 +0000

    Sun, Oct 29, 2023, 9:41 AM +05:45

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,19 +1,7 @@
 import sys
 try:
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 2 and (file_name[-4:] == ".csv")::
         file_name = sys.argv[1]
-        if (file_name[-4:] == ".csv"):
-            with open(file_name) as file:
-                lines = file.readlines()
-                for line in lines:
-                    if ((line.strip().startswith("#")) or  (line.strip() == "")):
-                        pass
-                    else:
-                        count +=1
-                print(count)
-
-        else:
-            sys.exit("Not a Python file")
     else:
         sys.exit("More or few arguments")
 except FileNotFoundError:
