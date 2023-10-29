1af82c82e9ba825a05377b2e0b2adb5631dbd1d4
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-29 08:43:00 +0000

    Sun, Oct 29, 2023, 2:28 PM +05:45

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,14 +1,14 @@
 import sys
 import csv
 
-# def get_filename(arguments):
-#     if len(arguments) != 3:
-#         if len(arguments) < 3:
-#             sys.exit('Too few command-line arguments')
-#         else:
-#             sys.exit('Too many command-line arguments')
-#     else:
-#          return sys.argv[1]
+def get_filename(arguments):
+    if len(arguments) != 3:
+        if len(arguments) < 3:
+            sys.exit('Too few command-line arguments')
+        else:
+            sys.exit('Too many command-line arguments')
+    else:
+         return sys.argv[1]
 
 def main():
     # file_name = get_filename(sys.argv)
@@ -23,8 +23,9 @@ def main():
 
     with open("after.csv","w") as file:
            writer = csv.DictWriter(file, fieldnames=["first","last","house"])
-           writer.writerow(fieldnames)
+           writer.writeheader()
            for student in students:
                     writer.writerow({"first": student['first'],"last": student['last'],"house": student['house']})
+
 if __name__ == '__main__':
     main()
