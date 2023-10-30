c0a1c47da6e3c9e60bce386f96032a6454e2b0ed
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-10-30 03:39:00 +0000

    Mon, Oct 30, 2023, 9:24 AM +05:45

--- rough/rough.py
+++ rough/rough.py
@@ -1,4 +1,10 @@
+students = []
+
 with open("student.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
