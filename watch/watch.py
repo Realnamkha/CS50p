8997bf93b369148ee46577f6b58bb0cbc6730fea
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-01 10:13:32 +0000

    Wed, Nov 1, 2023, 3:58 PM +05:45

--- watch/watch.py
+++ watch/watch.py
@@ -8,7 +8,9 @@ def main():
 
 def parse(s):
     s = s.strip()
-    matches = re.search
+    matches = re.search(r"^https?//(www.)?youtube\.com/embed/([A-Za-z0-9])$")
+    if matches:
+        print(matches.group(2))
 
 
 if __name__ == "__main__":
