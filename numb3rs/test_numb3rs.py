1163d381569e8d65768f9cafb7ae5ff97fc82345
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-01 09:57:04 +0000

    Wed, Nov 1, 2023, 3:42 PM +05:45

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -2,6 +2,7 @@ from numb3rs import validate
 
 def test_range():
     assert validate("0.0.0.0") == True
+    assert validate ('1') == False
     assert validate("255.255.255.255") == True
     assert validate("55.55.55.55") == True
     assert validate("555.55.555.55") == False
