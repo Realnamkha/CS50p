8dcb70cfa0f19cd55e015cc6c76a6cd60ed33e6c
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-02 04:15:58 +0000

    Thu, Nov 2, 2023, 10:00 AM +05:45

--- watch/watch.py
+++ watch/watch.py
@@ -12,7 +12,7 @@ def parse(s):
     # src_value = iframe_element.get('src')
     matches = re.search(r"<iframe.*?src=['\"](https?://(?:www\.)?youtube\.com/embed/([A-Za-z0-9]+))['\"].*</iframe>", s, re.IGNORECASE)
     if matches:
-        return f"https://youtu.be/{matches.group(1)}"
+        return f"https://youtu.be/{matches.group(2)}"
     else:
         return f"None"
 
