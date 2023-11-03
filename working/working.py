9dbbff82494b95b4e7b5a60337349aae0a8bc06a
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-03 05:14:00 +0000

    Fri, Nov 3, 2023, 10:59 AM +05:45

--- working/working.py
+++ working/working.py
@@ -11,7 +11,7 @@ def main():
 
 def convert(s):
     s=s.strip()
-    matches = re.fullmatch(r"((0?[1-9]|1[0-2]):[0-5][0-9] [AP][M]) to ((0?[1-9]|1[0-2]):[0-5][0-9] [AP][M])",s)
+    matches = re.fullmatch(r"((0?[1-9]|1[0-2]):?([0-5][0-9]|([00])?) [AP][M]) to ((0?[1-9]|1[0-2]):?([0-5][0-9]|([00]?)) [AP][M])",s)
     if matches:
         time1 = matches.group(1)
         time2 = matches.group(3)
