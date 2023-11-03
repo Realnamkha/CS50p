c1dca14341741cf01059fdccb1bea041850c708b
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-03 04:42:33 +0000

    Fri, Nov 3, 2023, 10:27 AM +05:45

--- working/working.py
+++ working/working.py
@@ -10,7 +10,7 @@ def convert(s):
     s=s.strip()
     matches = re.fullmatch(r"((0?[1-9]|1[0-2]):[0-5][0-9] [AP][M]) to ((0?[1-9]|1[0-2]):[0-5][0-9] [AP][M])",s)
     if matches:
-        return matches
+        return f"{matches.group(1)} to {matches.group(3)}"
     else:
         return ValueError
 
