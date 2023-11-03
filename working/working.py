fd3757b65c97f06123f9293f876ec951ce3f48d5
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-03 05:19:18 +0000

    Fri, Nov 3, 2023, 11:04 AM +05:45

--- working/working.py
+++ working/working.py
@@ -15,28 +15,37 @@ def convert(s):
     if matches:
         time1 = matches.group(1)
         time2 = matches.group(5)
-        if "AM" in time1 and "PM" in time2:
-            time1 = time1[:-2].strip()
-            hours,minutes = time1.split(":")
-            if hours == '12':
-                hours = '00'
-            time1 = f"{hours}:{minutes}"
-            time2 = time2[:-2].strip()
-            hours,minutes = time2.split(":")
-            hours = int(hours) + 12
-            time2 = f"{hours}:{minutes}"
-            return f"{time1} to {time2}"
+        if ":" in time1 and ":" in time2:
+            if "AM" in time1 and "PM" in time2:
+                time1 = time1[:-2].strip()
+                hours,minutes = time1.split(":")
+                if hours == '12':
+                    hours = '00'
+                time1 = f"{hours}:{minutes}"
+                time2 = time2[:-2].strip()
+                hours,minutes = time2.split(":")
+                hours = int(hours) + 12
+                time2 = f"{hours}:{minutes}"
+                return f"{time1} to {time2}"
+            else:
+                time2 = time2[:-2].strip()
+                hours,minutes = time2.split(":")
+                if hours == '12':
+                    hours = '00'
+                time2 = f"{hours}:{minutes}"
+                time1 = time1[:-2].strip()
+                hours,minutes = time1.split(":")
+                hours = int(hours) + 12
+                time1 = f"{hours}:{minutes}"
+                return f"{time1} to {time2}"
         else:
-            time2 = time2[:-2].strip()
-            hours,minutes = time2.split(":")
-            if hours == '12':
-                hours = '00'
-            time2 = f"{hours}:{minutes}"
-            time1 = time1[:-2].strip()
-            hours,minutes = time1.split(":")
-            hours = int(hours) + 12
-            time1 = f"{hours}:{minutes}"
-            return f"{time1} to {time2}"
+            if "AM" in time1 and "PM" in time2:
+                time1 = time1[:-2].strip()
+                if time1 == "12":
+                    time1 == "00"
+                time2 = time2[:-2].strip()
+                time2 = int(time2) + 12
+                return f"{time1} to {time2}"
     else:
          raise ValueError("Invalid time range format")
 
