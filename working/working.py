6fe36e5c37b2bbfb1404539ef204e63c11f8a3ee
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-03 04:35:16 +0000

    Fri, Nov 3, 2023, 10:20 AM +05:45

--- working/working.py
+++ working/working.py
@@ -8,12 +8,11 @@ def main():
 
 def convert(s):
     s=s.strip()
-    matches = re.fullmatch(r"[0-9]|1[1-2]:[0-5][0-9] [AM] to [0-9]|1[1-2]:[0-5][0-9] [PM]",s)
+    matches = re.fullmatch(r"([0-9]|1[1-2]:[0-5][0-9] [AM]) to ([0-9]|1[1-2]:[0-5][0-9] [PM])",s)
     if matches:
-        print(matches)
+        return matches
     else:
-        print("Not correct")
-
+        return ValueError
 
 ...
 
