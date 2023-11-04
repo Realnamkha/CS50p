64b00d75807e74259f351af4202ff1f653ff0c3e
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-04 05:09:11 +0000

    Sat, Nov 4, 2023, 10:54 AM +05:45

--- working/test_working.py
+++ working/test_working.py
@@ -2,7 +2,7 @@ from working import convert
 import pytest
 def test_extreme():
     assert convert("12:00 AM to 5:00 PM") == "00:00 to 17:00"
-    assert convert("12:00 AM to 5:00 PM") == "17:00 to 00:00"
+    assert convert("5:00 PM to 12:00 AM") == "17:00 to 00:00"
     assert convert("12 AM to 5 PM") == "00:00 to 17:00"
     assert convert("12:00 AM to 5:00 PM") != "12:00 to 17:00"
 
