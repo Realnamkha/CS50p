318203b888a0ee084342cf2d6f38284fdda09bbc
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-04 07:09:08 +0000

    Sat, Nov 4, 2023, 12:54 PM +05:45

--- response/response.py
+++ response/response.py
@@ -8,8 +8,10 @@ def main():
 
 def check_valid(email_address):
     try:
-        email_address = validators.email(email,allow_empty = True)
+        email_address = validators.email(email_address,allow_empty = True)
         is_email_address = checkers.is_email(email_address)
+        if is_email_address:
+            return "Valid"
     except errors.EmptyValueError:
         sys.exit("Invalid")
     except errors.InvalidEmailError:
