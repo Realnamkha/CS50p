cfdaeabcb91f02f5e4a0a0dd79c26a22b27c2264
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-04 07:07:41 +0000

    Sat, Nov 4, 2023, 12:52 PM +05:45

--- response/response.py
+++ response/response.py
@@ -6,8 +6,14 @@ def main():
     print(check_valid(input("Enter your email address: ")))
 
 
-def check_valid(email):
-    email_address = validators.email(email,allow_empty = True)
+def check_valid(email_address):
+    try:
+        email_address = validators.email(email,allow_empty = True)
+        is_email_address = checkers.is_email(email_address)
+    except errors.EmptyValueError:
+        sys.exit("Invalid")
+    except errors.InvalidEmailError:
+        sys.exit("Invalid")
 
 if __name__ == "__main__":
     main()
