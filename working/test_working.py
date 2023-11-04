63168c1fecc8446c0ad9525a8f0fc69eff80850c
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-04 05:08:26 +0000

    Sat, Nov 4, 2023, 10:53 AM +05:45

--- working/test_working.py
+++ working/test_working.py
@@ -1,13 +1,19 @@
 from working import convert
-
+import pytest
 def test_extreme():
-    assert("12:00 AM to 5:00 PM") == "00:00 to 17:00"
-    assert("12:00 AM to 5:00 PM") == "17:00 to 00:00"
-    assert("12 AM to 5 PM") == "00:00 to 17:00"
-    assert("12:00 AM to 5:00 PM") != "12:00 to 17:00"
+    assert convert("12:00 AM to 5:00 PM") == "00:00 to 17:00"
+    assert convert("12:00 AM to 5:00 PM") == "17:00 to 00:00"
+    assert convert("12 AM to 5 PM") == "00:00 to 17:00"
+    assert convert("12:00 AM to 5:00 PM") != "12:00 to 17:00"
 
 def test_input():
-    assert("12:00AM to 5:00PM") == raise ValueError
-    assert("9:60 AM to 5:60 PM") ==
-    assert("9 AM - 5 PM") ==
-    assert("09:00 AM - 17:00 PM") == 
+    with pytest.raises(ValueError):
+         convert('09:00 AM - 7:00 PM')
+    with pytest.raises(ValueError):
+         convert('9 AM - 5 PM')
+    with pytest.raises(ValueError):
+        convert('9:60 AM to 5:60 PM')
+
+def test_range():
+    with pytest.raises(ValueError):
+        convert("9:60 AM to 5:60 PM")
