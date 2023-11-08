b2e30c95ee4c91139424268a18fdd4d8b839c69c
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-08 14:51:10 +0000

    Wed, Nov 8, 2023, 8:36 PM +05:45

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -5,17 +5,18 @@ class Date:
         self.month = month
         self.day = day
 
-    def get_DOB():
+    @classmethod
+    def get_DOB(cls):
         year,month,day = input("Enter Your DOB :").strip().split("-")
         year = int(year)
         month = int(month)
         day = int(day)
-        return year,month,day
+        return cls(year,month,day)
 
-
-    def get_today():
+    @classmethod
+    def get_today(cls):
         year,month,day = str(date.today()).split("-")
-        return year,month,day
+        return cls(year,month,day)
 
     def __sub__(self, other):
         year = self.year + other.year
@@ -24,9 +25,10 @@ class Date:
         return Vault(galleons, sickles, knuts)
 
 def main():
-    year,month,day = get_DOB()
+    year,month,day = Date.get_DOB()
     date1 = Date(year,month,day)
-    year,month,day = get_today()
+    year,month,day = Date.get_today()
+    date2 = Date()
     date2 = Date(year,month,day)
     print(date1,date2)
 if __name__ == "__main__":
