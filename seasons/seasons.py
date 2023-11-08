34ac60817f6f78b700428e60ccd46d2c1c3d6e1f
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-08 04:07:08 +0000

    Wed, Nov 8, 2023, 9:52 AM +05:45

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,16 +1,20 @@
 from datetime import date
 class Date:
  ...
-def get_date():
+def get_DOB():
         year,month,day = input("Enter Your DOB :").strip().split("-")
         year = int(year)
         month = int(month)
         day = int(day)
         d = date(year, month, day)
         print(d)
+def get_today():
+    today = date.today()
+    print(today)
 
 def main():
-    get_date()
+    get_DOB()
+    get_today()
 
 
 if __name__ == "__main__":
