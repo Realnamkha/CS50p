f2fe2457297c73ba6a388d381966d968d85de07e
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-11 08:10:29 +0000

    Sat, Nov 11, 2023, 1:55 PM +05:45

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,3 +1,7 @@
 from fpdf import FPDF
 def main():
     name = input('Name: ')
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font('helvetica', 'B', 30)
+    pdf.cell(200, 50, 'CS50 Shirtificate', new_x="LMARGIN", new_y="NEXT", align='C')
