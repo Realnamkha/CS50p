264a46241c3ed5d1513029e3ea29b615fbc65d33
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-11 08:19:45 +0000

    Sat, Nov 11, 2023, 2:04 PM +05:45

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,7 +4,7 @@ def main():
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font('helvetica', 'B', 30)
-    pdf.cell(0, 60, 'CS50 Shirtificate', new_x="LMARGIN", new_y="NEXT", align='C')
+    pdf.cell(200, 50, 'CS50 Shirtificate', new_x="LMARGIN", new_y="NEXT", align='C')
     pdf.image('shirtificate.png', x=20, y=80, w=170)
     pdf.set_font_size(30)
     pdf.set_text_color(255,255,255)
