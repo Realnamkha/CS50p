d02bea650f19690412250dc581dfe5702f8315bd
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-16 16:12:48 +0000

    Thu, Nov 16, 2023, 9:57 PM +05:45

--- project/project.py
+++ project/project.py
@@ -11,28 +11,28 @@ def delete_expense():
     print("Delete expense")
 
 def main():
-    while True:
         print("Welcome to Expense Tracker")
         print("1. Record Expense ")
         print("2. View Expense ")
         print("3. Delete Expense ")
         print("4. Exit ")
-
+    while True:
         choice = input("Enter your choice\n")
 
         match choice:
             case "1":
                 record_expense()
+                break
             case "2":
                 view_expense()
+                break
             case "3":
                 delete_expense()
+                break
             case "4":
                 sys.exit("System exited ")
             case "_":
                 print("Enter a valid choice")
-                continue
-        break
 
 if __name__ == "__main__":
     main()
