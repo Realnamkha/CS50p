a8ee5abe1a19952fe1261fd4d26cfa31ed0ea54a
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-19 05:25:13 +0000

    Sun, Nov 19, 2023, 11:10 AM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -1,12 +1,12 @@
 import pytest
 from project import get_user_input,record_expense,view_expense,delete_expense,view_stats
 
-def test_get_user_input():
-    expense_amount = 1000
-    week_day = "sunday"
-    if category_input == "1":
-        category = "Food"
+def test_get_user_input(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "1000")  # Mock expense amount input
+    monkeypatch.setattr('builtins.input', lambda _: "sunday")  # Mock week_day input
+    monkeypatch.setattr('builtins.input', lambda _: "1")  # Mock category input
 
-    assert get_user_input() == expense_amount,week_day,category
+    expected_result = (1000, "sunday", "Food")
+    assert get_user_input() == expected_result
 
 
