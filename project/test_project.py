f4b91850c3df18c52e6cc61aa70456bff070f6a4
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-19 05:18:39 +0000

    Sun, Nov 19, 2023, 11:03 AM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -2,6 +2,10 @@ import pytest
 from project import get_user_input,record_expense,view_expense,delete_expense,view_stats
 
 def test_get_user_input():
-    
+    expense_amount = 1000
+    week_day = sunday
+    category_input = "1"
+
+    assert get_user_input() == expense_amount,week_day,"Food"
 
 
