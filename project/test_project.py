6e7a2eca03da07339e0ef043437eb04538005fb8
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-19 05:28:14 +0000

    Sun, Nov 19, 2023, 11:13 AM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -2,7 +2,7 @@ import pytest
 from project import get_user_input,record_expense,view_expense,delete_expense,view_stats
 
 def test_get_user_input(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: "1000")  # Mock expense amount input
+    monkeypatch.setattr('builtins.input', lambda _: 1000)  # Mock expense amount input
     monkeypatch.setattr('builtins.input', lambda _: "sunday")  # Mock week_day input
     monkeypatch.setattr('builtins.input', lambda _: "1")  # Mock category input
 
