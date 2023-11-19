468a0f8b8c0f406c116fa3d7df5c09db5b122d7b
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-19 05:11:31 +0000

    Sun, Nov 19, 2023, 10:56 AM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -1,6 +1,6 @@
 import pytest
-from project import record_expense,view_expense,delete_expense,view_stats
+from project import get_user_input,record_expense,view_expense,delete_expense,view_stats
 
 def test_record_expense():
-    
+
 
