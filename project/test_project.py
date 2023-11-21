4782307c89a16cf1e731d8228e6f5ae4c9545405
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-21 07:17:20 +0000

    Tue, Nov 21, 2023, 1:02 PM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -1,42 +1,34 @@
 
 import pytest
 from project import get_user_input, record_expense, view_expense, delete_expense, view_stats
-def test_get_user_input():
-    get_user_input.expense_amount = 1000
-    get_user_input.week_day = "sunday"
-    get_user_input.category_input = "1"  # Define and set the value of category_input
-    if get_user_input.category_input == "1":
-        category = "Food"
-    result = (get_user_input.expense_amount,get_user_input.week_day, category)
-    # Check the values against the expected result
-    expected_result = (1000, "sunday", "Food")
-    assert result == expected_result
+# def test_get_user_input():
+#     get_user_input.expense_amount = 1000
+#     get_user_input.week_day = "sunday"
+#     get_user_input.category_input = "1"  # Define and set the value of category_input
+#     if get_user_input.category_input == "1":
+#         category = "Food"
+#     result = (get_user_input.expense_amount,get_user_input.week_day, category)
+#     # Check the values against the expected result
+#     expected_result = (1000, "sunday", "Food")
+#     assert result == expected_result
 
-def test_get_user_input_not_valid():
-    get_user_input.expense_amount = "string"
-    get_user_input.week_day = "sunday"
-    get_user_input.category_input = "1"  # Define and set the value of category_input
-    if get_user_input.category_input == "1":
-        category = "Food"
-    result = (get_user_input.expense_amount,get_user_input.week_day, category)
-    # Check the values against the expected result
-    expected_result = (1000, "sunday", "Food")
-    assert result != expected_result
-
-
-def test_get_user_input_another(monkeypatch):
+def test_get_user_input(monkeypatch):
     mock_inputs = ["1000", "sunday", "1"]
-
     def mock_input(_):
         return mock_inputs.pop(0)
-
     monkeypatch.setattr('builtins.input', mock_input)
-
     result = get_user_input()
-
-    # Check the values against the expected result
     expected_result = (1000, "sunday", "Food")
     assert result == expected_result
+
+def test_get_user_input_not_valid(monkeypatch):
+    mock_inputs = ["Name", "sunday", "1"]
+    def mock_input(_):
+        return mock_inputs.pop(0)
+    monkeypatch.setattr('builtins.input', mock_input)
+    result = get_user_input()
+    expected_result = (1000, "sunday", "Food")
+    assert result != expected_result
 
 
 
