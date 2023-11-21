addbed8208a5faf8d0056dc358a0d54322c5ec9f
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-21 07:00:35 +0000

    Tue, Nov 21, 2023, 12:45 PM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -2,17 +2,11 @@
 import pytest
 from project import get_user_input, record_expense, view_expense, delete_expense, view_stats
 def test_get_user_input():
-    expense_amount = 1000
-    week_day = "sunday"
-    category_input = "1"  # Define and set the value of category_input
+    get_user_input.expense_amount = "1000"
+    get_user_input.week_day = "sunday"
+    get_user_input.category_input = "1"  # Define and set the value of category_input
     if category_input == "1":
         category = "Food"
-
-    # Call get_user_input and check its result
-    result = get_user_input()
-
-    # Unpack the result tuple
-    result_expense_amount, result_week_day, result_category = result
 
     # Check the values against the expected result
     expected_result = (1000, "sunday", "Food")
