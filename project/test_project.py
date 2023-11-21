51e2c2237626ec47a18f86bee408bc56a4ca74ea
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-21 06:32:28 +0000

    Tue, Nov 21, 2023, 12:17 PM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -3,11 +3,12 @@ from project import get_user_input, record_expense, view_expense, delete_expense
 
 def test_get_user_input(monkeypatch):
     # Mock user input for testing purposes
-    mock_inputs = ["1000", "sunday", "1"]
+    mock_inputs = iter(["1000", "sunday", "1"])
 
-    monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock expense amount input
-    monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock week_day input
-    monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock category input
+    def mock_input(prompt):
+        return next(mock_inputs)
+
+    monkeypatch.setattr('builtins.input', mock_input)
 
     expected_result = (1000, "sunday", "Food")
     assert get_user_input() == expected_result
