0e9222f066006d2c3795888a0d86d58b2d35f29f
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-21 06:51:55 +0000

    Tue, Nov 21, 2023, 12:36 PM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -1,7 +1,6 @@
 
 import pytest
 from project import get_user_input, record_expense, view_expense, delete_expense, view_stats
-
 def test_get_user_input(monkeypatch):
     # Mock user input for testing purposes
     mock_inputs = ["1000", "sunday", "1"]
@@ -12,6 +11,19 @@ def test_get_user_input(monkeypatch):
 
     expected_result = (1000, "sunday", "Food")
     assert get_user_input() == expected_result
+
+
+
+# def test_get_user_input(monkeypatch):
+#     # Mock user input for testing purposes
+#     mock_inputs = ["1000", "sunday", "1"]
+
+#     monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock expense amount input
+#     monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock week_day input
+#     monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock category input
+
+#     expected_result = (1000, "sunday", "Food")
+#     assert get_user_input() == expected_result
 
 # # Assuming that `get_user_input` is already tested, we can use it here.
 # def test_record_expense_successful(monkeypatch, capsys):
