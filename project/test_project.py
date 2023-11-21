a1e2132495e81df7454d1da7824e6ae38c151d46
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-21 06:36:45 +0000

    Tue, Nov 21, 2023, 12:21 PM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -1,24 +1,18 @@
+
 import pytest
 from project import get_user_input, record_expense, view_expense, delete_expense, view_stats
 
-def test_get_user_input(monkeypatch, capsys):
+def test_get_user_input(monkeypatch):
     # Mock user input for testing purposes
-    mock_inputs = iter(["1000", "sunday", "1"])
+    mock_inputs = ["1000", "sunday", "1"]
 
-    def mock_input(prompt):
-        return next(mock_inputs)
+    monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock expense amount input
+    monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock week_day input
+    monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock category input
 
-    monkeypatch.setattr('builtins.input', mock_input)
+    expected_result = (1000, "sunday", "Food")
+    assert get_user_input() == expected_result
 
-    # Call the function
-    result = get_user_input()
-
-    # Check the captured output
-    captured = capsys.readouterr()
-    assert captured.out == "***Categories***\n1 Food\n2 Stationary\n3 Travel\n4 Entertainment\n5 Others\n"
-
-    # Check the result
-    assert result == (1000, "sunday", "Food")
 
 
 
