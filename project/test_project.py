f11fd0c5a17c69b870ee893c385457b36e4e3947
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-21 07:08:51 +0000

    Tue, Nov 21, 2023, 12:53 PM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -12,6 +12,17 @@ def test_get_user_input():
     expected_result = (1000, "sunday", "Food")
     assert result == expected_result
 
+def test_get_user_input_not_valid():
+    get_user_input.expense_amount = "string"
+    get_user_input.week_day = "sunday"
+    get_user_input.category_input = "1"  # Define and set the value of category_input
+    if get_user_input.category_input == "1":
+        category = "Food"
+    result = (get_user_input.expense_amount,get_user_input.week_day, category)
+    # Check the values against the expected result
+    expected_result = (1000, "sunday", "Food")
+    assert result != expected_result
+
 
 
 
