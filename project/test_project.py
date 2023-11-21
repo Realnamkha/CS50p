30ecd4101262c4cee0b0721fe3a8a47ff227b192
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-21 07:24:55 +0000

    Tue, Nov 21, 2023, 1:09 PM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -1,4 +1,4 @@
-
+from unittest.mock import patch
 import pytest
 from project import get_user_input, record_expense, view_expense, delete_expense, view_stats
 # def test_get_user_input():
@@ -32,27 +32,8 @@ def test_get_user_input_not_valid(monkeypatch):
 
 
 
-
-# def test_get_user_input(monkeypatch):
-#     # Mock user input for testing purposes
-#     mock_inputs = ["1000", "sunday", "1"]
-
-#     monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock expense amount input
-#     monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock week_day input
-#     monkeypatch.setattr('builtins.input', lambda _: mock_inputs.pop(0))  # Mock category input
-
-#     expected_result = (1000, "sunday", "Food")
-#     assert get_user_input() == expected_result
-
-# # Assuming that `get_user_input` is already tested, we can use it here.
-# def test_record_expense_successful(monkeypatch, capsys):
-#     # Mock user input for testing purposes
-#     monkeypatch.setattr('project.get_user_input', lambda: (1000, 'sunday', 'Food'))
-
-#     # Capture the printed output
-#     record_expense()
-#     captured = capsys.readouterr()
-
-#     # Assert that the success message is printed
-#     assert "**Added Successfully**" in captured.out
+def test_record_expense_valid_input():
+    with patch('project.get_user_input', return_value=(1000, 'sunday', 'Food')):
+        with patch('builtins.open'):
+            record_expense()
 
