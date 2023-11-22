de794e447d384be328adbd39783ed36e98e8bdf8
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-22 14:41:59 +0000

    Wed, Nov 22, 2023, 8:26 PM +05:45

--- project/test_project.py
+++ project/test_project.py
@@ -30,13 +30,16 @@ def test_get_user_input(monkeypatch):
 #     expected_result = (1000, "sunday", "Food")
 #     assert result != expected_result
 
-def test_get_user_input_invalid_amount():
+def test_get_user_input_invalid_amount(monkeypatch):
     mock_inputs = [None, "sunday", "1"]
     def mock_input(_):
         return mock_inputs.pop(0)
     monkeypatch.setattr('builtins.input', mock_input)
     result = get_user_input()
-    
+    with patch('builtins.open'):
+        with patch('builtins.print') as mock_print:
+                record_expense()
+    mock_print.assert_called_once_with("Invalid expense amount. Please enter a valid number.")
 
 
 
@@ -50,13 +53,13 @@ def test_record_expense_valid_input():
     # Assertions for valid input
     mock_print.assert_called_once_with("**Added Successfully**")
 
-def test_record_expense_invalid_input():
-    # Test when expense_amount is None
-    with patch('project.get_user_input', return_value=(None, 'sunday', 'Food')):
-        with patch('builtins.open'):
-            # Capture the printed output
-            with patch('builtins.print') as mock_print:
-                record_expense()
+# def test_record_expense_invalid_input():
+#     # Test when expense_amount is None
+#     with patch('project.get_user_input', return_value=(None, 'sunday', 'Food')):
+#         with patch('builtins.open'):
+#             # Capture the printed output
+#             with patch('builtins.print') as mock_print:
+#                 record_expense()
 
-    # Assertions for invalid input
-    mock_print.assert_called_once_with("Invalid expense amount. Please enter a valid number.")
+#     # Assertions for invalid input
+#     mock_print.assert_called_once_with("Invalid expense amount. Please enter a valid number.")
