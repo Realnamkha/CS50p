3b7e0cbed44e7ebfdb4839c5c0faac3edf0a37e6
Realnamkha <81818669+Realnamkha@users.noreply.github.com>
2023-11-24 06:06:25 +0000

    Fri, Nov 24, 2023, 11:51 AM +05:45

--- rough/rough.py
+++ rough/rough.py
@@ -1,33 +1,9 @@
-import tkinter as tk
-from tkinter import ttk
+students = [
+    {"name": "Hermoine", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
 
-def on_category_select(event):
-    selected_category = category_var.get()
-    print("Selected Category:", selected_category)
-
-# Create the main window
-root = tk.Tk()
-root.title("Category Selection")
-
-# Create a StringVar to hold the selected category
-category_var = tk.StringVar()
-
-# Create a Label widget
-label = tk.Label(root, text="Select a category:")
-
-# Create a Combobox (dropdown menu) widget
-category_combobox = ttk.Combobox(root, textvariable=category_var)
-category_combobox['values'] = ("Food", "Stationary", "Travel", "Entertainment", "Others")
-
-# Set the default value for the Combobox
-category_combobox.set("Food")
-
-# Bind the event handler to the category Combobox
-category_combobox.bind("<<ComboboxSelected>>", on_category_select)
-
-# Pack the widgets into the window
-label.pack(pady=10)
-category_combobox.pack(pady=10)
-
-# Start the Tkinter event loop
-root.mainloop()
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
